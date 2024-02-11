cbebe7aa4a4e17bfe06ed28507be1e0cbfea2bff
ddmilado <adebayodamilola@gmail.com>
2024-02-11 19:39:04 +0400

    latest

--- app/components/WelcomeForm.tsx
+++ app/components/WelcomeForm.tsx
@@ -72,7 +72,7 @@ const WelcomeForm: React.FC<WelcomeFormProps> = ({
     <div className="border-gray-500 bg-gray-200 sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border-2 sm:w-full">
       <div className="flex flex-col space-y-4 p-7 sm:p-10">
         <h1 className="text-lg font-semibold text-black">
-          Welcome to Agent42!
+          Welcome to Growthify Agents!
         </h1>
         <form className="flex flex-col space-y-3">
           <input
@@ -102,10 +102,10 @@ const WelcomeForm: React.FC<WelcomeFormProps> = ({
             </button>
             <button
               type="button"
-              onClick={() => setAssistantModel('gpt-3.5-turbo-1106')}
+              onClick={() => setAssistantModel('ft:gpt-3.5-turbo-1106:growthify::8qwhKSVZ')}
               className={`p-1 border border-gray-400 rounded-md ${assistantModel === 'gpt-3.5-turbo-1106' ? 'bg-blue-500 text-white' : ''}`}
             >
-              GPT-3.5
+              Custom Model
             </button>
           </div>
         </form>
--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   redirects: async () => {
     return [
       {
-        source: "/github",
-        destination: "https://github.com/admineral/OpenAI-Assistant-API-Chat",
-        permanent: true,
-      },
-      {
         source: "/deploy",
         destination: "https://open-ai-assistant-api-chat.vercel.app",
         permanent: true,
